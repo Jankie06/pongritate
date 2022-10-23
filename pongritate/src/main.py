d8721c1292cabf03245ce289d5129d175324fe8f
Jadon Wright <jadonwright0603@gmail.com>
2022-10-23 17:27:34 -0400

    Saving before I break everything. 💔

--- pongritate/src/main.py
+++ pongritate/src/main.py
@@ -34,14 +34,16 @@ PLAYBUTTONCOLOR = (55, 55, 55)
 BACKGROUNDCOLOR = (43, 43, 43)
 
 TITLE = pygame.image.load(f"{os.getcwd()}\\pongritate\\assets\\title.png")
-titlePositionX, titlePositionY = (screen.get_width() / 2) - (720 / 2), (height / 3)-300
+titlePositionX, titlePositionY = (screen.get_width() / 2) - (720 / 2), (
+    height / 3
+) - 300
 desiredY = titlePositionY
 
 playButtonPressed = False
 toggle = True
 limit = 100
 loop_count = 0
-while not(playButtonPressed):
+while not (playButtonPressed):
     loop_count += 1
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -49,7 +51,8 @@ while not(playButtonPressed):
 
     if (
         pygame.mouse.get_pos()[0] >= (screen.get_width() / 2) - (playButtonSize / 2)
-        and pygame.mouse.get_pos()[0] <= ((screen.get_width() / 2) - (playButtonSize / 2)) + playButtonSize
+        and pygame.mouse.get_pos()[0]
+        <= ((screen.get_width() / 2) - (playButtonSize / 2)) + playButtonSize
         and pygame.mouse.get_pos()[1] >= 440
         and pygame.mouse.get_pos()[1] <= 523
     ):
@@ -60,16 +63,16 @@ while not(playButtonPressed):
         playButtonSize = playButtonSize + ((200 - playButtonSize) / 50)
 
     if loop_count > limit:
-        toggle = not(toggle)
+        toggle = not (toggle)
         limit = loop_count + 200
     elif toggle == True:
         desiredY += 1
     elif toggle == False:
         desiredY -= 1
     titlePositionY = titlePositionY + ((desiredY - titlePositionY) / 150)
-    
+
     screen.fill((BACKGROUNDCOLOR))
-    
+
     titlePositionX = (screen.get_width() / 2) - (720 / 2)
 
     pygame.draw.rect(
@@ -96,67 +99,94 @@ while not(playButtonPressed):
     pygame.display.update()
 
 
-
+SCREEN = screen.get_rect()
 
 PADDLE = pygame.image.load(f"{os.getcwd()}\\pongritate\\assets\\paddle.png")
-BALL = pygame.image.load(f"{os.getcwd()}\\pongritate\\assets\\ball.png")  
+BALL = pygame.image.load(f"{os.getcwd()}\\pongritate\\assets\\ball.png")
 
 score = 0
-highScore = json.load(open(f"{os.getcwd()}\\pongritate\\data\\savedata.json"))["highScore"]
+highScore = json.load(open(f"{os.getcwd()}\\pongritate\\data\\savedata.json"))[
+    "highScore"
+]
+
 
 mouseX, mouseY, clicked = 0, 0, False
 paddleX, paddleY = 0, 0
-ballX, ballY = 0, 0
-ballXVel, ballYVel = 0, 10
+ballX, ballY = 50, 0
+ballXVel, ballYVel, randomVel = 0, 10, 0
 
 
 def tick(code):
     global mouseX, mouseY, clicked
-    
+
     if code == "screen":
         pygame.display.flip()
-        screen.fill(BACKGROUNDCOLOR)
         pygame.time.Clock().tick(60)
+        
+        screen.fill(BACKGROUNDCOLOR)
 
     if code == "physics":
         print(0)
 
-    if code == "mouse":      
+    if code == "mouse":
         mouseX = pygame.mouse.get_pos()[0]
         mouseY = pygame.mouse.get_pos()[1]
         if pygame.mouse.get_pressed() == (1, 0, 0):
             clicked = True
 
+
 def paddle(x, y):
     screen.blit(PADDLE, (x, y))
+
 
 def ball(x, y):
     screen.blit(BALL, (x, y))
 
+
 def update():
-    global ballX, ballY, ballXVel, ballYVel
-    
+    global ballX, ballY, ballXVel, ballYVel, colliding, paddleColl, ballColl, randomVel, screen
+
     tick("mouse")
-    
+
     paddleX, paddleY = mouseX, (screen.get_height() / 4) * 3
     paddle(paddleX, paddleY)
-    
-    ballX -= ballXVel
+
+    ballX += ballXVel
     ballY -= ballYVel
+
     
-    if not(ballXVel <= 0):
-        ballXVel -= 1
+    ballXVel = (randomVel - ballX) / 50
     ballYVel -= 1
-    
+
     ball(ballX, ballY)
+
+    ballColl, paddleColl, colliding = (
+        pygame.Rect(ballX, ballY, 100, 100),
+        pygame.Rect(paddleX, paddleY, 100, 100),
+        False,
+    )
+
+    colliding = pygame.Rect.colliderect(ballColl, paddleColl)
+
+    if ballX >= screen.get_width()-100 or ballX <= 0:
+        if ballX > 0:
+            randomVel -= 100
+        elif ballX < 0:
+            randomVel += 100
+        
+    if colliding:
+        ballYVel = 30
+        randomVel = random.randint(-100, screen.get_width()+100)
+        #ballXVel = randomVel
+        
 
 
 while True:
-    
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-    
+
     update()
-    
+
     tick("screen")
